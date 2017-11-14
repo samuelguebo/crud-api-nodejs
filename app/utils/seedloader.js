b3f7014d44aa6d6f3a22780bda145132fb88a9ce
African Hope <samuelguebo@gmail.com>
2017-11-14 18:55:43 +0000

    Seed:users object array

--- app/utils/seedloader.js
+++ app/utils/seedloader.js
@@ -1,6 +1,37 @@
+// Requiring models
+var User = require('../models.user.js');
+
+/**
+ * Seeding the Database
+ */
 var SeedLoader = function (request, response, next) {
+    
+    const users = [
+        {
+            firstName: 'Shino',
+            lastName: 'Aburame',
+            email: 'shino@aburame.com',
+            password: 'shinoaburame'
+        },{
+            firstName: 'Shikamaru',
+            lastName: 'Nara',
+            email: 'shikamaru@nara.com',
+            password: 'shikamarunara'
+        },{
+            firstName: 'Ino',
+            lastName: 'yamanaka',
+            email: 'ino@yamanaka.com',
+            password: 'inoyamanaka'
+        }
+        
+    ];
+    
+    
     console.log("Entered SeedLoader Middleware");
     next();
 }
+
+
+
 
 module.exports = SeedLoader;
