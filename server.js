4681146efe137d8580514421183e423230f9eb5b
African Hope <samuelguebo@gmail.com>
2017-11-18 21:03:59 +0000

    using jwt for handling tokens

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// Get the necessary packages
 var express     = require('express');
 var app         = express();
 var bodyParser  = require('body-parser');
+var jwt         = require('jsonwebtoken'); // used for handling tokens
 
 // Server port 
 var port = 5000;
