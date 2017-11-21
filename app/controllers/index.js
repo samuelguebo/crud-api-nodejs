d1b55cd17016b91c7eda069ccde0aa6eedede259
African Hope <samuelguebo@gmail.com>
2017-11-21 19:49:35 +0000

    middleware structure

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,10 @@
 var express = require('express'),
     router = express.Router();
 
+// Routes
 router.use('/posts', require('./posts'));
 router.use('/users', require('./users'));
 router.use('/categories', require('./categories'));
-router.use('/authenticate', require('./authenticate'));
+router.use('/authentication', require('./authenticate'));
 
 module.exports = router;
